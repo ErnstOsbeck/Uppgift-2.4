8e4e128cc7fea2b70cdc37e3005d1b6f320f58a8
ErnstOsbeck <143172129+ErnstOsbeck@users.noreply.github.com>
2023-09-15 12:56:54 +0200

    Add project files.

---
+++ InlämningsUppgift/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace uppgift_2._4
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hur mycket känar den första?");
+            int lön1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Hur mycket känar den andra?");
+            int lön2 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Hur mycket känar den tredje?");
+            int lön3 = int.Parse(Console.ReadLine());
+            int medel = (lön1 + lön2 + lön3) / 3;
+            Console.WriteLine("Medellönen är " + medel + "kronor");
+            Console.Read();
+        }
+    }
+}
